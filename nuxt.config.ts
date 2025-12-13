252edaab1fbd2ed4c62f544c20afbb80c9ea16cd
danangace <danangbahari89@gmail.com>
2025-12-13 10:01:05 +0700

    chore: add tailwind css to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
@@ -6,4 +8,8 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
   },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/tailwind.css"],
 });
