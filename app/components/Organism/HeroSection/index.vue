47bac7e370ccf7af1fdd80e436ebc7f5324b67df
haritsbachtiar <ahmad.harits1@gmail.com>
2026-02-19 21:26:06 +0700

    update hero banner

--- app/components/Organism/HeroSection/index.vue
+++ app/components/Organism/HeroSection/index.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const items = [
-  "https://images.unsplash.com/photo-1607237138185-eedd9c632b0b?q=80&w=2070&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=2086&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=2070&auto=format&fit=crop",
+  // "https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=2086&auto=format&fit=crop",
+    "/img/banner-1.png",
+  // "https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=2070&auto=format&fit=crop",
 ];
 </script>
 
 <template>
-  <div class="relative h-[50vh] md:h-[70vh] w-full">
+  <div class="relative w-full">
     <UCarousel
       v-slot="{ item }"
       :items="items"
@@ -25,14 +25,14 @@ const items = [
       <div
         class="absolute inset-0 bg-black/30 flex items-center justify-center"
       >
-        <div class="text-center text-white px-4">
+        <!-- <div class="text-center text-white px-4">
           <h1 class="text-4xl md:text-6xl font-bold mb-4">
             STIE Tamansiswa Jakarta
           </h1>
           <p class="text-xl md:text-2xl">
             Membangun Generasi Unggul dan Berkarakter
           </p>
-        </div>
+        </div> -->
       </div>
     </UCarousel>
   </div>
