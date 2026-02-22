efffe9b718acf0140fcb7d2eb0d60400e6092bae
haritsbachtiar <ahmad.harits1@gmail.com>
2026-02-22 21:28:53 +0700

    update navbar

--- app/components/Organism/Navbar/index.vue
+++ app/components/Organism/Navbar/index.vue
@@ -17,7 +17,7 @@ const items = computed<NavigationMenuItem[]>(() => {
         { label: "Visi dan Misi", icon: "i-lucide-target", to: "/profil/visi-misi" },
         { label: "Sejarah STIE Taman Siswa", icon: "i-lucide-book-open", to: "/profil/tentang" },
         { label: "Struktur Organisasi", icon: "i-lucide-network", to: "/profil/struktur-organisasi" },
-        { label: "Staff Pengajar", icon: "i-lucide-users", to: "/profil/staff-pengajar" },
+        { label: "Tenaga Pengajar", icon: "i-lucide-users", to: "/profil/staff-pengajar" },
         // { label: "Kalender Akademik", icon: "i-lucide-calendar", to: "/profil/kalender-akademik" },
         // { label: "Kerjasama", icon: "i-lucide-handshake", to: "/profil/kerjasama" },
         // { label: "Akreditasi", icon: "i-lucide-award", to: "/profil/akreditasi" },
